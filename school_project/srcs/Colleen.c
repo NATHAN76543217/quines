f01666a9f81b6f969548c6745a3b4f19c9393a43
Nathan <maximenathan@hotmail.fr>
2023-04-08 19:13:02 +0200

    executables conformes au sujet; ready to turn-in

--- school_project/srcs/Colleen.c
+++ school_project/srcs/Colleen.c
@@ -1,3 +1,6 @@
+/*
+	Program that dump its own source code
+*/
 # include <stdio.h>
 # include <stdlib.h>
 
@@ -116,27 +119,25 @@ char	*make_program(char	*s)
 /*
 	test the function
 */
-int	tester( void )
+int	payload( void )
 {
-	dprintf(2, "test done\n");
-	return 2;
+	return 0;
 }
 
 int main()
 {
-	char *includes = "# include <stdio.h>\n# include <stdlib.h>\n\n";
+	char *includes = "/*\n\tProgram that dump its own source code\n*/\n# include <stdio.h>\n# include <stdlib.h>\n\n";
 	char *common = "size_t\tft_strlen(const char *s)\n{\n\tint\ti;\n\n\ti = 0;\n\twhile (s[i])\n\t\ti++;\n\treturn i;\n}\n\ntypedef struct s_list{\n\tvoid\t\t\t*malloc;\n\tstruct s_list\t*next;\n}\tt_list;\n\nt_list\t\t**getUniqMalloc( void )\n{\n\tstatic t_list* malloced = NULL;\n\treturn &malloced;\n}\n\nvoid\tdelUniqMalloc( void )\n{\n\tt_list**\tmalloced = getUniqMalloc();\n\tt_list*\t\ttmp = NULL;\n\n\twhile ((*malloced) != NULL)\n\t{\n\t\ttmp = (*malloced);\n\t\tfree((*malloced)->malloc);\n\t\t(*malloced) = (*malloced)->next;\n\t\tfree(tmp);\n\t}\n}\n\nchar\t*uniq_malloc( unsigned int size)\n{\n\tt_list**\tmalloced = getUniqMalloc();\n\tt_list*\t\ttmp = NULL;\n\n\tchar * new = malloc(size);\n\tif (new == NULL)\n\t\treturn NULL;\n\n\tif (*malloced == NULL)\n\t{\n\t\t*malloced = malloc(sizeof(t_list));\n\t\tif (*malloced == NULL)\n\t\t{\n\t\t\tfree(new);\n\t\t\treturn NULL;\n\t\t}\n\t\t(*malloced)->malloc = new;\n\t\t(*malloced)->next = NULL;\n\t}\n\telse\n\t{\n\t\ttmp = *malloced;\n\t\twhile (tmp->next != NULL)\n\t\t{\n\t\t\ttmp = tmp->next;\n\t\t}\n\t\ttmp->next = malloc(sizeof(t_list));\n\t\tif (tmp->next == NULL)\n\t\t{\n\t\t\tfree (new);\n\t\t\treturn NULL;\n\t\t}\n\t\ttmp = tmp->next;\n\t\ttmp->malloc = new;\n\t\ttmp->next = NULL;\n\t}\n\treturn new; \n}\n\nchar\t*make_program(char\t*s)\n{\n\tint\t\tlen;\n\tint\t\ti;\n\tchar\t*new;\n\tchar\t*p;\n\n\ti = 0;\n\tlen = ft_strlen(s);\n\tnew = uniq_malloc(len * 2 + 1);\n\tif (new == NULL)\n\t\treturn NULL;\n\tp = new;\n\twhile (i < len)\n\t{\n\t\tif (s[i] == '\\n' || s[i] == '\\t' || s[i] == '\\\"' || s[i] == '\\\\')\n\t\t{\n\t\t\t*p = '\\\\';\n\t\t\tp++;\n\t\t}\n\t\tif (s[i] == '\\n')\n\t\t\t*p = 'n';\n\t\telse if (s[i] == '\\t')\n\t\t\t*p = 't';\n\t\telse if (s[i] == '\\\"')\n\t\t\t*p = '\\\"';\n\t\telse if (s[i] == '\\\\')\n\t\t\t*p = '\\\\';\n\t\telse\n\t\t\t*p = s[i];\n\t\tp++;\n\t\ti++;\n\t}\n\t*p = '\\0';\n\treturn new;\n}\n";
-	char *functions[1];
-	functions[0] = "/*\n\ttest the function\n*/\nint\ttester( void )\n{\n\tdprintf(2, \"test done\\n\");\n\treturn 2;\n}\n\n";
-	char *format_string = "%s%s\n%sint main()\n{\n\tchar *includes = \"%s\";\n\tchar *common = \"%s\";\n\tchar *functions[1];\n\tfunctions[0] = \"%s\";\n\tchar *format_string = \"%s\";\n\t/*\n\t\tCall the tester of the function\n\t*/\n\ttester();\n\n\t/* Display includes */\n\n\tdprintf(1, format_string, includes, common, functions[0], make_program(includes), make_program(common), make_program(functions[0]), make_program(format_string));\n\tdelUniqMalloc();\n\treturn 0;\n}\n";
+	char *payloads = "/*\n\ttest the function\n*/\nint\tpayload( void )\n{\n\treturn 0;\n}\n\n";
+	char *format_string = "%s%s\n%sint main()\n{\n\tchar *includes = \"%s\";\n\tchar *common = \"%s\";\n\tchar *payloads = \"%s\";\n\tchar *format_string = \"%s\";\n\t/*\n\t\tCall the payload of the function\n\t*/\n\tpayload();\n\n\t/* Display includes */\n\n\tdprintf(1, format_string, includes, common, payloads, make_program(includes), make_program(common), make_program(payloads), make_program(format_string));\n\tdelUniqMalloc();\n\treturn 0;\n}\n";
 	/*
-		Call the tester of the function
+		Call the payload of the function
 	*/
-	tester();
+	payload();
 
 	/* Display includes */
 
-	dprintf(1, format_string, includes, common, functions[0], make_program(includes), make_program(common), make_program(functions[0]), make_program(format_string));
+	dprintf(1, format_string, includes, common, payloads, make_program(includes), make_program(common), make_program(payloads), make_program(format_string));
 	delUniqMalloc();
 	return 0;
 }
--- school_project/srcs/Sully.c
+++ school_project/srcs/Sully.c
@@ -4,8 +4,11 @@
 # include <unistd.h>
 # include <libft.h>
 
-# define PAYLOAD int payload() { return 0;}
-# define STRINGS "# include <stdio.h>\n# include <stdlib.h>\n# include <fcntl.h>\n# include <unistd.h>\n# include <libft.h>\n\n# define PAYLOAD %s\n# define STRINGS \"%s\"\n\n# define COUNTER %d\n# define STR_HELPER(x) #x \n# define STRINGIFY(VALUE) STR_HELPER(VALUE)\n\n\n# define CODE %s\n\n# define START %s\n\n# define END(x) %s\n\n# define FT(x) CODE PAYLOAD int main(){ int fd = -1; int counter = COUNTER - 1; char *format_string = STRINGS; START /* Display */dprintf(fd, format_string, STRINGIFY(PAYLOAD), make_program(STRINGS), counter, STRINGIFY(CODE), STRINGIFY(START), STRINGIFY(END())); END(0) wrdestroy();return 0;}\n\n\n/*\n\tThis Macro will declare a main then write all this code page into a file named 'Grace_kid.c'\n*/\nFT()\n"
+# ifndef LIBFT_PATH
+# define LIBFT_PATH
+#endif
+
+# define STRINGS "# include <stdio.h>\n# include <stdlib.h>\n# include <fcntl.h>\n# include <unistd.h>\n# include <libft.h>\n\n# ifndef LIBFT_PATH\n# define LIBFT_PATH\n#endif\n\n# define STRINGS %s\n\n# define COUNTER %d\n# define STR_HELPER(x) #x \n# define STRINGIFY(VALUE) STR_HELPER(VALUE)\n\n\n# define CODE %s\n\n# define START %s\n\n# define END(x) %s\n\n# define FT(x) CODE int main(){ int fd = -1; int counter = COUNTER; char *format_string = STRINGS; char* current_filename = __FILE__; START /* Display */dprintf(fd, format_string, ft_quote(make_program(STRINGS)), counter, STRINGIFY(CODE), STRINGIFY(START), STRINGIFY(END())); close(fd);system(ft_strjoin(\"clang -Wall -Werror -Wextra -o Sully\", ft_strjoin(ft_itoa(counter), ft_strjoin(\" -DLIBFT_PATH=\\\"\\\"\", ft_strjoin(ft_quote(STRINGIFY(LIBFT_PATH)), ft_strjoin(\"\\\"\\\" -DOSX -I\", ft_strjoin(STRINGIFY(LIBFT_PATH), ft_strjoin(\"/lib/libft/includes -L\", ft_strjoin(STRINGIFY(LIBFT_PATH), ft_strjoin(\"/lib/libft -lft \", filename))))))))));END(0) wrdestroy();return 0;}\n\n\n/*\n\tThis Macro will create a slitly different version of himself which will reproduce itself\n*/\nFT()\n"
 
 # define COUNTER 5
 # define STR_HELPER(x) #x 
@@ -14,14 +17,14 @@
 
 # define CODE typedef struct str_list{void *malloc; struct str_list *next;} str_list; char *make_program(char *s) {int len = ft_strlen(s); int i; char *new; char *p; i = 0; new = wrmalloc(len * 2 + 1); if (new == ((void*)0)) return ((void*)0); p = new; while (i < len) {if (s[i] == '\n' || s[i] == '\t' || s[i] == '\"' || s[i] == '\\') {*p = '\\'; p++;} if (s[i] == '\n') *p = 'n'; else if (s[i] == '\t') *p = 't'; else if (s[i] == '\"') *p = '\"'; else if (s[i] == '\\') *p = '\\'; else *p = s[i]; p++; i++;} *p = '\0'; return new;}
 
-# define START char *filename = ft_strjoin("Sully_", ft_strjoin(ft_itoa(counter), ".c")); if ((fd = open(filename, 0x0002 | 0x0400 | 0x0200 , 0000700 | 0000070)) < 0) return 0;
+# define START if (!ft_strcmp(current_filename, ft_strjoin("Sully_", ft_strjoin(ft_itoa(counter), ".c"))))counter--;if (counter < 0) return 0; char *filename = ft_strjoin("Sully_", ft_strjoin(ft_itoa(counter), ".c")); if ((fd = open(filename, 0x0002 | 0x0400 | 0x0200 , 0000700 | 0000070)) < 0) return 0;
 
-# define END(x) close(fd);system(ft_strjoin("gcc -Wall -Werror -Wextra -o Sully", ft_strjoin(ft_itoa(counter), ft_strjoin(" -DOSX -Ilib/libft/includes -Llib/libft -lft ", filename))));if (counter <= 0) return 0; system(ft_strjoin("./Sully", ft_itoa(counter)));
+# define END(x) if (counter <= 0) return 0;system(ft_strjoin("./Sully", ft_itoa(counter)));
 
-# define FT(x) CODE PAYLOAD int main(){ int fd = -1; int counter = COUNTER - 1; char *format_string = STRINGS; START /* Display */dprintf(fd, format_string, STRINGIFY(PAYLOAD), make_program(STRINGS), counter, STRINGIFY(CODE), STRINGIFY(START), STRINGIFY(END())); END(0) wrdestroy();return 0;}
+# define FT(x) CODE int main(){ int fd = -1; int counter = COUNTER; char *format_string = STRINGS; char* current_filename = __FILE__; START /* Display */dprintf(fd, format_string, ft_quote(make_program(STRINGS)), counter, STRINGIFY(CODE), STRINGIFY(START), STRINGIFY(END())); close(fd);system(ft_strjoin("clang -Wall -Werror -Wextra -o Sully", ft_strjoin(ft_itoa(counter), ft_strjoin(" -DLIBFT_PATH=\"\"", ft_strjoin(ft_quote(STRINGIFY(LIBFT_PATH)), ft_strjoin("\"\" -DOSX -I", ft_strjoin(STRINGIFY(LIBFT_PATH), ft_strjoin("/lib/libft/includes -L", ft_strjoin(STRINGIFY(LIBFT_PATH), ft_strjoin("/lib/libft -lft ", filename))))))))));END(0) wrdestroy();return 0;}
 
 
 /*
-	This Macro will declare a main then write all this code page into a file named 'Grace_kid.c'
+	This Macro will create a slitly different version of himself which will reproduce itself
 */
 FT()
